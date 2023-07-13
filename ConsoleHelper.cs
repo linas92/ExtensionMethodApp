290b60343ada2ebddd3a71bf8ef1d625912a787d
Linas Norkus <linasnorkus92@gmail.com>
2023-07-13 21:53:09 +0300

    Made a method to check if the integers the user entered is valid. Fixed the spacing which ran away.

--- ConsoleHelper.cs
+++ ConsoleHelper.cs
@@ -8,12 +8,23 @@ namespace ExtensionMethodApp
         {
             string output = "";
 
-
             while (string.IsNullOrWhiteSpace(output))
             {
                 Console.WriteLine(message);
-
                 output = Console.ReadLine();
+            }
+            return output;
+        }
+
+        public static int RequestInt(this int message)
+        {
+            int output = 0;
+            bool isValidAge = false;
+
+            while (isValidAge == false)
+            {
+                Console.WriteLine(message);
+                isValidAge = int.TryParse(Console.ReadLine(), out output);
             }
             return output;
         }
